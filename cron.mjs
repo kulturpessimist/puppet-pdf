2f36f2c95c93764c287c183ba77bc50dc9be8e45
kulturpessimist <alex@schedler.co>
2023-02-21 09:24:29 +0100

    better cron

---
+++ cron.mjs
@@ -0,0 +1,28 @@
+import { ofetch } from "ofetch";
+
+const TOKEN =
+  process.env.SCALINGO_TOKEN ||
+  "tk-us-qPRDoJOpTbETcXPL4hMtx-6tjShQcduT8hY-OxQdS7snbmVS";
+
+const { token } = await ofetch("https://auth.scalingo.com/v1/tokens/exchange", {
+  method: "POST",
+  headers: [
+    ["Accept", "application/json"],
+    ["Content-Type", "application/json"],
+    ["Authorization", "Basic " + Buffer.from(":" + TOKEN).toString("base64")],
+  ],
+});
+
+const response = await ofetch.raw(
+  "https://api.osc-fr1.scalingo.com/v1/apps/puppet-pdf/restart",
+  {
+    method: "POST",
+    headers: [["Authorization", "Bearer " + token]],
+  }
+);
+
+if (response.status === 202) {
+  console.log("OK, Service restarted.");
+} else {
+  console.error("Error, Service _not_ restarted");
+}
