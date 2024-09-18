8a5e0f89ad4d725c57ea944f868c497db1a655ee
Alex Schedler <alex@schedler.co>
2024-09-18 12:23:14 +0200

    chore: added sentry environment

--- sentry.mjs
+++ sentry.mjs
@@ -3,6 +3,7 @@ import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  environment: process.env.SENTRY_ENVIRONMENT,
   integrations: [nodeProfilingIntegration()],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
